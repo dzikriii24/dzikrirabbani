0b115e60359ad0321f8b06e9a22b4ec82c325932
dzikriii24 <dzikrirabbani2401@gmail.com>
2026-01-03 00:01:49 +0700

    anomali ditemukan kah?

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Wajib untuk Tailwind v4
 import path from 'path'
 import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [react(),  viteCompression({
-      algorithm: 'gzip', // atau 'brotliCompress' kalau mau Brotli
+  plugins: [
+    react(),
+    tailwindcss(), // Plugin Tailwind v4 dimasukkan di sini
+    viteCompression({
+      algorithm: 'gzip',
       ext: '.gz',
-      threshold: 1024, // cuma file >1kb yang di-compress
-      deleteOriginFile: false // kalau mau hapus file asli, set true
-    })],
-  base: '/', // Nama repo kamu
+      threshold: 1024,
+      deleteOriginFile: false
+    })
+  ],
+  base: '/', // Pastikan ini '/' untuk root domain di Vercel
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
